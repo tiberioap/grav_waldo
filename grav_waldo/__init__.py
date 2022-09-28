5838fa73479309d088feb7e1b8f634862ba6c05e
Tibério Pereira <tiberio@fisica.ufrn.br>
2022-09-27 22:09:02 -0300

    first commit

---
+++ grav_waldo/__init__.py
@@ -0,0 +1,4 @@
+#__author__ = "Tibério A. Pereira"
+#__email__ = "tiberio@fisica.ufrn.br"
+#__orcid__ = "https://orcid.org/0000-0003-1856-6881"
+#__version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ import setuptools
 with open('requirements.txt', 'r') as f:
     install_requires = f.read().splitlines()
 
-setuptools.setup(name='waldo',
+setuptools.setup(name='grav_waldo',
                  version='0.1.0',
                  author='Tibério A. Pereira',
                  description='waveform anomaly detector',
-                 packages=['waldo'],
+                 packages=['grav_waldo'],
                  install_requires=install_requires,
                  classifiers=["Programming Language :: Python :: 3",
-                             "Operating System :: OS Independent"])
+                              "Operating System :: OS Independent"])
 
